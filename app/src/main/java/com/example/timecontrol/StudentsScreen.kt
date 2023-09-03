49a1446c6b161bc99cba66df1d967840928ddc57
ReptilianEye <piotr.rzadkowski@outlook.com>
2023-09-03 15:21:50 +0200

    students list done

--- app/src/main/java/com/example/timecontrol/StudentsScreen.kt
+++ app/src/main/java/com/example/timecontrol/StudentsScreen.kt
@@ -1,45 +1,38 @@
 package com.example.timecontrol
 
-import android.widget.Toast
-import androidx.compose.foundation.background
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Divider
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.timecontrol.calendaritem.CalendarItem
-import com.example.timecontrol.data.dto.Quote
 import com.example.timecontrol.database.Student
-import com.example.timecontrol.quote.Quote
-import com.example.timecontrol.statstile.StatsTile
 import com.example.timecontrol.studentslistitem.StudentsListItem
-import com.example.timecontrol.ui.theme.Blue20
-import java.time.format.DateTimeFormatter
+import com.example.timecontrol.ui.theme.Blue10
+import com.example.timecontrol.ui.theme.BlueLogo
+import com.example.timecontrol.ui.theme.White80
 
 @Composable
 fun StudentsScreen(students: List<Student>) {
-    LazyColumn(
-        modifier = Modifier.fillMaxWidth(),
-        verticalArrangement = Arrangement.spacedBy(3.dp)
-    ) {
+    Box(modifier = Modifier.fillMaxSize())
+    {
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(3.dp)
+
+            //uncomment when database connected
+        ) {
 //        students.forEachIndexed { index, student ->
 //            item(index) {
 //                StudentsListItem(
@@ -50,23 +43,43 @@ fun StudentsScreen(students: List<Student>) {
 //                    departureDate = student.departureDate.format(DateTimeFormatter.ofPattern("dd.MM"))
 //                        .toString(),
 //                    onClick = {},
+//        background = if (it % 2 == 0) White80 else Blue10,
 //                    modifier = Modifier
 //                        .width(390.dp)
 //                        .height(102.dp)
 //                )
 //            }
+            //comment when database connected
+            items(20) {
+                StudentsListItem(
+                    level = "D",
+                    name = "Tomek",
+                    telNo = "123456789",
+                    placeOfStay = "Chałupy",
+                    departureDate = "10.09",
+                    onClick = {},
+                    background = if (it % 2 == 0) White80 else Blue10,
+                    modifier = Modifier.padding(2.dp)
+//                probably redundant - need to try on other device
+//                modifier =
+//                    Modifier
+//                        .fillMaxWidth()
+//                        .fillMaxHeight(0.5f).height(102.dp)
+                )
 
-        items(20) {
-            StudentsListItem(
-                level = "D",
-                name = "Tomek",
-                telNo = "123456789",
-                placeOfStay = "Chałupy",
-                departureDate = "10.09",
-                onClick = {},
-                modifier = Modifier
-                    .width(390.dp)
-                    .height(102.dp)
+            }
+        }
+        FloatingActionButton(
+            modifier = Modifier
+                .padding(16.dp)
+                .size(75.dp)
+                .align(Alignment.BottomEnd),
+            onClick = { /*TODO*/ },
+            containerColor = BlueLogo
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.add_vector),
+                contentDescription = "Add Student"
             )
         }
     }
