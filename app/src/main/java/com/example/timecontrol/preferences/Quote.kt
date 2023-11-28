a9e7cb59528bf33dba9ef4729111cefbd0faa6aa
ReptilianEye <piotr.rzadkowski@outlook.com>
2023-11-28 08:16:43 +0100

    Fix quote composable and display in app

--- app/src/main/java/com/example/timecontrol/preferences/Quote.kt
+++
@@ -1,6 +0,0 @@
-package com.example.timecontrol.preferences
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class Quote(val quote: String = "", val author: String = "", val category: String = "")
